5b8cadd00640232acfc94b04a880d5a14c5faadf
William <billy.ryan.rom@gmail.com>
2020-03-12 10:03:28 -0400

    Add 500 status for Posts Requests/Add incomplete PUT REQUEST

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,28 +3,53 @@ const express = require('express');
 const Posts = require("./postDb")
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   Posts.get()
     .then(post => res.json(post))
-    .catch(next)
+    .catch(err => {
+      console.log("Error: ", err);
+      res.status(500).json({
+        err: "The post could not be removed"
+      })
+    })
 });
 
 router.get('/:id', validatePostId(), (req, res) => {
+  console.log(req.post)
+  if(req.post){
   res.status(200).json(req.post)
+  } else {
+    res.status(500)
+    .json({
+      message: "The specified Post could not be found"
+    })
+  }
 });
 
-router.delete('/:id', validatePostId(), (req, res, next) => {
+router.delete('/:id', validatePostId(), (req, res) => {
   Posts.remove(req.params.id)
     .then(() => {
       res.status(200).json({
         message: "The post is deleted"
       })
     })
-    .catch(next)
+    .catch(err => {
+      console.log("Error: ", err);
+      res.status(500).json({
+        message: "The post could not be removed"
+      })
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId(), (req, res) => {
+  console.log(req.body)
+  Posts.update(req.params.id, req.body)
+  .then(() => {
+      res.status(200).json({
+        message: "Post is updated"
+      })
+  })
+  .catch()
 });
 
 // custom middleware
