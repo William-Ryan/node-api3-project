8d73cd9e8fdb1a683ae0efdd77af1cea6a9273f6
William <billy.ryan.rom@gmail.com>
2020-03-12 07:59:55 -0400

    Import user and post routers to server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const userRouter = require("./users/userRouter.js")
+const postRouter = require("./posts/postRouter.js") 
+
 const server = express();
+server.use("/api/users", logger, userRouter)
+server.use("/api/posts", logger, postRouter)
 
 server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
