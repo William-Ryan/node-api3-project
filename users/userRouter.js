fbf6d0f07ef16e0fc15ff1188b081a5fcef7dca7
William <billy.ryan.rom@gmail.com>
2020-03-12 10:06:55 -0400

    Add validateUser middleware to userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -45,7 +45,13 @@ function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(!req.body) {
+    res.status(400).json({ message: "missing user data" })
+  } else if(req.body.name === "") {
+    res.status(400).json({ message: "missing required name field" })
+  }else{
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
